cb96f07d583eaef770a44e44ba00ffaec71f0db7
kevinbdsouza <kdsouza1496@gmail.com>
2021-05-16 11:39:53 +0530

    update

--- src/train_fns/data_utils.py
+++ src/train_fns/data_utils.py
@@ -82,7 +82,7 @@ def load_hic_samples(chr, cfg, hic_data):
     unsplit_ids = np.vstack(idx_agg)
     unsplit_ids = np.concatenate((np.full((unsplit_ids.shape[0], 1), chr), unsplit_ids), 1).astype('int')
 
-    return unsplit_ids, input_pos, hic_values
+    return unsplit_ids, hic_values, input_pos
 
 
 def load_hic_data(chr, cell, cfg):
@@ -104,8 +104,8 @@ def get_hic_loader_chr(chr, cell, cfg):
 
 def get_hic_loader(cell, cfg):
     # build dataset
-    input_pos_agg = torch.empty(1, cfg.chunk_length, 2)
-    values_agg = torch.empty(1, cfg.chunk_length)
+    input_pos_agg = torch.empty(0, cfg.chunk_length, 2)
+    values_agg = torch.empty(0, cfg.chunk_length)
     unsplit_ids_agg = []
 
     # for chr in list(range(1, 11)) + list(range(12,23)):
@@ -121,7 +121,7 @@ def get_hic_loader(cell, cfg):
     # save data
     torch.save(input_pos_agg, cfg.processed_data_dir + 'input_pos.pth')
     torch.save(values_agg, cfg.processed_data_dir + 'hic_values.pth')
-    torch.save(ids_agg, cfg.processed_data_dir + 'unsplit_ids.pth')
+    torch.save(unsplit_ids_agg, cfg.processed_data_dir + 'unsplit_ids.pth')
 
     # build dataloader
     datacomb = torch.utils.data.TensorDataset(input_pos_agg, values_agg)
